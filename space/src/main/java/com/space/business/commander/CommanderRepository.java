0a41a912d07425329678750730f973cfd45bfeea
striggle <stbigrigg@gmail.com>
2018-10-27 19:55:34 -0400

    initial commit

---
+++ space/src/main/java/com/space/business/commander/CommanderRepository.java
@@ -0,0 +1,7 @@
+package com.space.business.commander;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommanderRepository extends CrudRepository<Commander, Integer> {
+
+}
